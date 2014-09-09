e4e10f69b7cbeb29ce0d36eeb2695363fb3eba25
Jay <Jay.Woo@students.olin.edu>
2014-09-08 20:54:59 -0400

    Added files from Android Studio

--- app/src/main/java/lab0/jwoo/lab0/MainActivity.java
+++ app/src/main/java/lab0/jwoo/lab0/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +14,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = (Button) findViewById(R.id.my_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
